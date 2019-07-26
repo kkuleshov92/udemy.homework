12e52d29ada5e77e5b3fd0adc5c5bf4066d29c36
kkuleshov92 <nstkuleshov1@gmail.com>
2019-07-26 13:38:58 +0300

    task10 completed

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -1,6 +1,8 @@
 window.addEventListener('DOMContentLoaded', function(){
 
     'use strict';
+
+    //Tabs
     let tab = document.querySelectorAll('.info-header-tab'),
         info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent');
@@ -34,4 +36,61 @@ window.addEventListener('DOMContentLoaded', function(){
             }
         };
     });
+
+    //Timer
+    let deadLine = '2018-07-27T00:00:00';
+
+    function getTimeRemaining(endTime) {
+        let t = Date.parse(endTime) - Date.parse(new Date()),
+            seconds = Math.floor((t / 1000) % 60),
+            minutes = Math.floor(((t / 1000) / 60) % 60),
+            hours = Math.floor((t / (1000 * 60 * 60)));
+
+        return {
+            'total' : t,
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds
+        };
+    }
+
+    function setClock(id, endTime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds');
+
+        if (Date.parse(endTime) < Date.parse(new Date())) {
+            hours.textContent = '00';
+            minutes.textContent = '00';
+            seconds.textContent = '00';
+            return false;
+        }
+
+        let timeInterval = setInterval(updateClock, 1000);
+
+        
+
+        function updateClock() {
+            let t = getTimeRemaining(endTime);
+            hours.textContent = t.hours;
+            if (hours.textContent.length < 2) {
+                hours.textContent = '0' + hours.textContent;
+            }
+            minutes.textContent = t.minutes;
+            if (minutes.textContent.length < 2) {
+                minutes.textContent = '0' + minutes.textContent;
+            }
+            seconds.textContent = t.seconds;
+            if (seconds.textContent.length < 2) {
+                seconds.textContent = '0' + seconds.textContent;
+            }
+
+            if(t.total < 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('timer', deadLine);
 });
